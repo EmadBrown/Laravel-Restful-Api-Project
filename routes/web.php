93ada844cc65cf5318f49d8f71c6109cb9921dbc
EmadBrown <emadbrown2013@gmail.com>
2017-10-29 01:39:11 +0200

    Fix AdminLoginController and the view of the admin/login

--- routes/web.php
+++ routes/web.php
@@ -34,12 +34,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->group(function(){
-Route::get('/', 'AdminController@index')->name('admin.dashbard');
-Route::get('/login', 'auth\AdminController@showLoginForm')->name('admin.login');
-Route::post('/login', 'auth\AdminController@login')->name('admin.login.submit');
+Route::get('/', 'AdminController@index')->name('admin.dashboard');
+Route::get('/login', 'auth\AdminLoginController@showLoginForm')->name('admin.login');
+Route::post('/login', 'auth\AdminLoginController@login')->name('admin.login.submit');
 });
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
